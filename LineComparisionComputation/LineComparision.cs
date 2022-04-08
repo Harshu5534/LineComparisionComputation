2d0863f32546695f5b7fa884188ad831d0fc33f6
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-08 13:46:33 +0530

    [Harshal]Add:LineComparision Lenth Calculate Feature Added

--- LineComparisionComputation/LineComparision.cs
+++ LineComparisionComputation/LineComparision.cs
@@ -21,7 +21,6 @@ namespace LineComparisionComputation
         public double CalculateLength()
         {
             double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine(result);
             return result;
         }
 

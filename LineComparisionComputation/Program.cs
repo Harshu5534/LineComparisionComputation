e9a82299f3bfcdcfeebc4481924115d478fa2e8e
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-08 13:35:24 +0530

    [Harshal]Add:LineComparision Added

---
+++ LineComparisionComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LineComparisionComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LineComparision Computation = new LineComparision(20, 45, 56, 57);
+            double Line1 = Computation.CalculateLength();
+        }
+    
+    }
+}
